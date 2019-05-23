5ffdca873438689cfd6f3dd3f4f046b01fd58c17
inderrsingh <inder679@hotmail.com>
2019-05-23 15:23:48 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+const myApp = {};
+
+myApp.key = "JltfITEhSLswBpcoq5t1OZxFECWVYYvCEvTQnrTa";
+
+myApp.init = function () {
+    $.ajax({
+        url: "https://api.nasa.gov/planetary/apod",
+        method: "GET",
+        dataType: "json",
+        data: {
+            api_key: myApp.key,
+            date: "2019-05-21"
+        }
+    }).then(function (result) {
+        console.log(result)
+        $("div").append(`<img src="${result.url}" alt=""/>`)
+    });
+};
+
+
+
+$(function () {
+    myApp.init();
+})
+
+
+        
+                        
+
+
+
+
