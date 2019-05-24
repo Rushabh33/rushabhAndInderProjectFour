289d6d0c5fdd667aa8d8450c5e0d50c374bc893a
Rushabh Parekh <rushabh.parekh.11@gmail.com>
2019-05-23 21:08:22 -0400

    api display in html

--- script.js
+++ script.js
@@ -1,32 +1,56 @@
-// const myApp = {};
-
-// myApp.key = "JltfITEhSLswBpcoq5t1OZxFECWVYYvCEvTQnrTa";
-
-// myApp.init = function () {
-//     $.ajax({
-//         url: "https://api.nasa.gov/planetary/apod",
-//         method: "GET",
-//         dataType: "json",
-//         data: {
-//             api_key: myApp.key,
-//             date: "2019-05-21"
-//         }
-//     }).then(function (result) {
-//         console.log(result)
-//         $("div").append(`<img src="${result.url}" alt=""/>`)
-//       //$("img").src=result.url
-//     });
-// };
+const myApp = {};
 
 
-// Button.on(click, function(){
-//     $("img").src=result.url
-// })
+
+myApp.init = function () {
+
+myApp.key = "JltfITEhSLswBpcoq5t1OZxFECWVYYvCEvTQnrTa";
+myApp.userDate = $("#userDate")
+myApp.date;
+myApp.today = new Date(); 
 
 
-// $(function () {
-//     myApp.init();
-// })
+
+    // function dateFormat(){
+    //     let today = new Date();
+    //     let formattedDate = `${today.getFullYear()}-${today.getMonth()+1}-${today.getDate()}`
+    //     myApp.userDate.val(formattedDate)
+    // }
+
+    // dateFormat();
+
+
+    $.ajax({
+        url: "https://api.nasa.gov/planetary/apod",
+        method: "GET",
+        dataType: "json",
+        data: {
+            api_key: myApp.key,
+            date: myApp.date
+        }
+    }).then(function (result) {
+        console.log(result);
+        $(".displayImage img").attr("src", `${result.url}`)
+    });
+
+    
+
+
+    
+};
+
+
+
+
+
+$(function () {
+    myApp.init();
+    myApp.userDate.change(function(){
+        myApp.date = myApp.userDate.val();
+        myApp.init();
+    })
+
+})
 
 
         
